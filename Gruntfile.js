6536b9299a1cbaa3b4946b2c21bb4232bdce09a2
Siddhartha Gudipati <siddu.siddhartha@gmail.com>
2014-10-01 12:14:06 -0700

    Added memcached and mongodb icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -108,7 +108,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 9001,
         open: true,
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
